70e8b35b65d2662028420b79cc2223b08f0bd54d
Byungjin-Kim <composerjins@gmail.com>
2023-05-01 23:31:00 -0500

    wip

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/hello', (req, res) => {
+    res.json('hello, world');
+});
+
+server.use('*', (req, res) => {
+    res.json({ message: 'API is UP!' });
+});
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
